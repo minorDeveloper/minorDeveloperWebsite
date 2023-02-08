74561e77f9872f8e88631ad31598809bdd66a553
minorDeveloper <theminordeveloper@outlook.com>
2023-02-08 21:46:31 +0000

    Update favicon

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230208.214227';
+const cacheName = 'chirpy-20230208.214620';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
